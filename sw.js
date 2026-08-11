bbc1c01d17c0b47c0729af41c1bb9da26c3d5e44
Walid Messafer <walidymessafer@gmail.com>
2026-08-11 04:23:39 +0100

    Publish offline ENG2111 revision site

---
+++ sw.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = 'eng2111-exam-rescue-v4';
+const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './icon.svg', './asset-manifest.json'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(Promise.all([
+    caches.open(CACHE_NAME),
+    fetch('./asset-manifest.json').then(response => response.json()),
+  ]).then(([cache, assets]) => cache.addAll([...new Set([...APP_SHELL, ...assets])])));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  const url = new URL(event.request.url);
+  if (url.origin !== self.location.origin) return;
+
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
+      if (response.ok) {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+      }
+      return response;
+    }).catch(() => event.request.mode === 'navigate' ? caches.match('./index.html') : undefined))
+  );
+});
